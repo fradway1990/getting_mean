8c710d8716cac9801142b71cc26dc108c99b16e0
Frank Collins <frankcollinsdesigns@gmail.com>
2018-01-03 06:36:05 -0500

    Connected to mongodb

---
+++ app_server/models/locations.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+
+var openingTimeSchema = new mongoose.Schema({
+                days: {type: String, required: true},
+                opening: String,
+                closing: String,
+                closed: {type: Boolean, required:true}
+});
+
+var reviewSchema = new mongoose.Schema({
+              author: {type:String, required:true},
+              rating: {type: Number, "default":0, min:0, max:5},
+              createdOn: {type:Date, required:true},
+              reviewText: {type:String, required:true}
+});
+
+
+
+var locationSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  address: {type: String, required: true},
+  rating: {type: Number, "default":0, min:0, max:5},
+  facilities: [String],
+  coords: {type: [Number],index: '2dsphere'},
+  openingTimes:[openingTimeSchema],
+  reviews: [reviewSchema]
+});
+//@(name of model,schema to use,(Mongodb collection name | optional));
+mongoose.model('Location',locationSchema,'locations');
